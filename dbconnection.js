01749e7add1ec363dc3c88e791abf3325a406fd4
sudhirbhivshet <s.bhivshet@gmail.com>
2018-01-08 21:40:04 +0530

    commit again

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'shops'
+  host: 'sql12.freemysqlhosting.net',
+  user: 'sql12214287',
+  password: 'fexu5rNr6x',
+  database: 'sql12214287'
 });
 module.exports = connection;
